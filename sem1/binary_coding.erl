996adbcbaa7ea4b84cf01f0186aa9eecaee92456
Steven Arnow <s@rdw.se>
2013-11-13 12:37:04 +0100

    Am I done yet? ;_;

--- sem1/binary_coding.erl
+++ sem1/binary_coding.erl
@@ -1,13 +1,14 @@
 -module(binary_coding).
 -export([get_bit/1]).
+-export([get_bit2/1]).
 
 get_bit(Num, Pos) -> case Pos =< 1 of
 	true -> [];
 	false -> (case (Num rem Pos) >= (Pos div 2) of
 		true -> [1|get_bit(Num, Pos div 2)];
 		false -> [0|get_bit(Num, Pos div 2)]
-		end)
-	end.
+	end)
+end.
 
 find_pos(Num, Pos) -> case Pos =< Num of
 	true -> find_pos(Num, Pos * 2);
@@ -16,3 +17,12 @@ end.
 
 
 get_bit(Num) -> find_pos(Num, 2).
+
+
+list_unchr(Lst) -> case Lst == [] of
+	true -> [];
+	false ->  [hd(Lst) - 48 | list_unchr(tl(Lst))]
+end.
+
+
+get_bit2(Num) -> list_unchr(integer_to_list(Num, 2)).
