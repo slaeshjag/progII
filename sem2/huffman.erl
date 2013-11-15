da3bbe5027035bb19eb24609d545be9c14ed7341
Steven Arnow <s@rdw.se>
2013-11-15 20:13:50 +0100

    woop pattern matching

--- sem2/huffman.erl
+++ sem2/huffman.erl
@@ -3,28 +3,25 @@
 -export([list_seek/2]).
 
 %% list_seek - Skips elements until a new one is found
-list_seek(Element, Skip) -> case length(Element) > 0 of
-	true -> (case hd(Element) == Skip of
-		true -> list_seek(tl(Element), Skip);
-		false -> Element end);
-	false -> Element
+list_seek([], _) -> [];
+list_seek([H|T], Skip) -> case H == Skip of
+	true -> list_seek(T, Skip);
+	false -> [H|T]
 end.
 
 
 %% list_count - Counts how many times an element is repeated
-list_count(Element, Start) -> case length(Element) > 1 of
-	true -> (case hd(Element) == Start of
-		true -> 1 + list_count(tl(Element), Start);
-		false -> 0 end);
-	false -> 1
+list_count([], _) -> 0;
+list_count([H|T], Start) -> case H == Start of
+	true -> 1 + list_count(T, Start);
+	false -> 0
 end.
 
 %% huffman_prob - Takes a sorted list
-huffman_prob(Element, Seek) -> case length(Element) > 0 of
-	true -> (case Seek =/= hd(Element) of
-		true -> huffman_prob(list_seek(tl(Element), hd(Element)), Seek);
-		false -> list_count(Element, hd(Element)) end);
-	false -> 0
+huffman_prob([], _) -> 0;
+huffman_prob([H|T], Seek) -> case Seek =/= H of
+	true -> huffman_prob(list_seek(T, H), Seek);
+	false -> list_count([H|T], H) 
 end.
 
 
