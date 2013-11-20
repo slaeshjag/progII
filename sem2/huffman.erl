6dd2e2648025f3df1537c4acdf8c8c6c987174c7
Steven Arnow <s@rdw.se>
2013-11-20 19:27:31 +0100

    Den kan avkoda huffman också

--- sem2/huffman.erl
+++ sem2/huffman.erl
@@ -3,6 +3,12 @@
 -export([list_seek/2]).
 -export([table/1]).
 -export([encode/2]).
+-export([decode/2]).
+-export([test/0]).
+
+sample() -> "the quick brown fox jumps over the lazy dog this is a sample text that we will use when we build up a table we will only handle lower case letters and no punctuation symbols the frequency will of course not represent english but it is probably not that far off".
+%sample() -> "En stor rund hund med mycket jobbiga tecken, som kan drastiskt minska kompressionsgraden av text. Räksmörgås. Vad drar du för slutsats av detta? Allt jag vet är att huffmankodningen i sig verkar fungera rätt okej".
+
 
 %% list_seek - Skips elements until a new one is found
 list_seek([], _) -> [];
@@ -58,3 +64,22 @@ find_encoding(Element, [_|T]) -> find_encoding(Element, T).
 
 encode([], _) -> [];
 encode([H|T], {endec, _, Ta}) -> find_encoding(H, Ta) ++ encode(T, {endec, 0, Ta}).
+
+%% Decode %%
+
+find_decoding([0|T], {branch, _, _, L, _}, Otr) -> find_decoding(T, L, Otr);
+find_decoding([1|T], {branch, _, _, _, R}, Otr) -> find_decoding(T, R, Otr);
+find_decoding(T, {leaf, Byte, _}, Otr) -> [Byte|find_decoding(T, Otr, Otr)];
+find_decoding([], _, _) -> [].
+
+decode(T, {endec, [Tr], _}) -> find_decoding(T, Tr, Tr).
+
+
+%% Test %%
+
+test() -> Sample = sample(),
+	Table = table(Sample),
+	Seq = encode(Sample, Table),
+	Text = decode(Seq, Table),
+	io:format("Before: ~w bits, after: ~w bits~n Compression ratio: ~w~n", [length(Text) * 8, length(Seq), length(Seq) / length(Text) / 8]),
+	Text.
